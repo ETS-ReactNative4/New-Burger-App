daeff0e373cec1c06dbbd6d38cfab4dd19db2756
tanveerul-hoque <tanveerctg2014@gmail.com>
2019-03-19 11:54:33 +0600

    Updates

---
+++ precache-manifest.7672f243f9016f300092f0f9d6591aee.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9a833a7a34bbe775e11af314736b636",
+    "url": "/tanveerctg/New-Burger-App/static/media/j.e9a833a7.png"
+  },
+  {
+    "revision": "34d8ed03b0469ee9307d",
+    "url": "/tanveerctg/New-Burger-App/static/css/main.21a2ebdc.chunk.css"
+  },
+  {
+    "revision": "b73c1f9b81699836b46d6e2370b79c7d",
+    "url": "/tanveerctg/New-Burger-App/static/media/egg.b73c1f9b.png"
+  },
+  {
+    "revision": "266d303ee002aa3f7282",
+    "url": "/tanveerctg/New-Burger-App/static/js/1.266d303e.chunk.js"
+  },
+  {
+    "revision": "a6ed7e0aefc63095effe",
+    "url": "/tanveerctg/New-Burger-App/static/js/runtime~main.a6ed7e0a.js"
+  },
+  {
+    "revision": "ba25e5960ff6c7ce76a90b5439411073",
+    "url": "/tanveerctg/New-Burger-App/static/media/1.ba25e596.jpeg"
+  },
+  {
+    "revision": "297084bc8714e34bea00e94becf3f241",
+    "url": "/tanveerctg/New-Burger-App/static/media/logo.297084bc.png"
+  },
+  {
+    "revision": "ed7d87af0feeeaa3e94d2d3b3d61c921",
+    "url": "/tanveerctg/New-Burger-App/static/media/beef.ed7d87af.png"
+  },
+  {
+    "revision": "34d8ed03b0469ee9307d",
+    "url": "/tanveerctg/New-Burger-App/static/js/main.34d8ed03.chunk.js"
+  },
+  {
+    "revision": "645f3dc637f704efc49a3b22a2393f62",
+    "url": "/tanveerctg/New-Burger-App/static/media/salad.645f3dc6.png"
+  },
+  {
+    "revision": "4cb39bf30746068754a899b1d9b12fab",
+    "url": "/tanveerctg/New-Burger-App/static/media/cheese.4cb39bf3.png"
+  },
+  {
+    "revision": "f367a43cd6a812a1e7cee0077393c9af",
+    "url": "/tanveerctg/New-Burger-App/static/media/chicken.f367a43c.png"
+  },
+  {
+    "revision": "38ac7189776812f9107ffece938c6866",
+    "url": "/tanveerctg/New-Burger-App/static/media/all.38ac7189.png"
+  },
+  {
+    "revision": "6bede7e3dae39d66ea992d14bb052821",
+    "url": "/tanveerctg/New-Burger-App/static/media/stockout.6bede7e3.jpg"
+  },
+  {
+    "revision": "266d303ee002aa3f7282",
+    "url": "/tanveerctg/New-Burger-App/static/css/1.6cbdfda6.chunk.css"
+  },
+  {
+    "revision": "0f15d5465d512287f73ee71332ee778f",
+    "url": "/tanveerctg/New-Burger-App/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/tanveerctg/New-Burger-App/precache-manifest.7672f243f9016f300092f0f9d6591aee.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/tanveerctg/New-Burger-App/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
